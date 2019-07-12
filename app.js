b8a11c4c2baecdf61c5d331e6629709ba0779f2f
Kevin Auer <kvn35124@uab.edu>
2019-07-12 17:14:28 -0500

    added a sing button

--- app.js
+++ app.js
@@ -1,33 +1,40 @@
 let friends = ["Kevin", "Kyle", "Stephen", "Jack", "Bill"];
-var word = "lines";
+// var word = "lines";
 var count = 99;
 
+let btn = document.createElement('button');
+document.body.appendChild(btn);
+let btnText = document.createTextNode('Sing!');
+btn.appendChild(btnText);
 
+btn.addEventListener('click', function () {
+    
 
-
-for (let i = 0; i < friends.length; i++) {
-    console.log(friends[i]);
-    while (count > 0) {
-        if (count == 1) {
-            var word = "line"
-        }
-        console.log(count + " " + word + " of code in the file");
-        console.log(count + " " + word + " of code,");
-        console.log(friends[i] + " strikes one out, clears it all out,");
-        count = count - 1;
-        if (count > 0) {
+    for (let i = 0; i < friends.length; i++) {
+        console.log(friends[i]);
+        var word = "lines";
+        while (count > 0) {
             if (count == 1) {
                 var word = "line"
             }
-            console.log(count + " " + word + " lines of code in the file.");
-        } else {
-            if (count < 1) {
-                var word = "lines"
+            console.log(count + " " + word + " of code in the file");
+            console.log(count + " " + word + " of code,");
+            console.log(friends[i] + " strikes one out, clears it all out,");
+            count = count - 1;
+            if (count > 0) {
+                if (count == 1) {
+                    var word = "line"
+                }
+                console.log(count + " " + word + " of code in the file.");
+            } else {
+                if (count < 1) {
+                    var word = "lines"
+                }
+                console.log("No more " + word + " of code in the file.");
             }
-            console.log("No more " + word + " of code in the file.");
         }
     }
-}
+})
 
 
 
